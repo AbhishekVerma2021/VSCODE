78b4e02386ae1443a61b29a33284fd616a349e0f
AbhishekVerma2021 <2001avverma@gmail.com>
2022-02-05 20:01:55 +0530

    commit on 'Amazon'

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+function navbar()
+{
+    return(<>
+        <nav >
+        <ul className="navbar">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>);
+}
